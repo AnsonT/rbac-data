3e5de48b7c2744b2d342007441cfabef9be28381
Anson Tsao <tsaoa@acm.org>
2020-01-28 14:05:53 -0800

    Cleaned up package.json

--- src/lib/tests/global_setup.js
+++
@@ -1,8 +0,0 @@
-// const knex = require('./db').default
-
-module.exports = async () => {
-  // await knex.migrate.rollback()
-  // await knex.migrate.latest()
-  // console.info('DB environment setup')
-  // await knex.destroy()
-}
--- src/lib/tests/global_teardown.js
+++
@@ -1,6 +0,0 @@
-// const knex = require('./db').default
-
-module.exports = async () => {
-  // await knex.destroy()
-  // console.info('DB environment teardown')
-}
